ceca133ae8f4f0e8ec4cbe3ea4951e990ba4b97b
Ercan Karakaya <karakayaercan71@gmail.com>
2023-10-29 01:25:45 +0300

    Text Blocks

--- src/com/ercan/TextBlocks/TextBlocks.java
+++ src/com/ercan/TextBlocks/TextBlocks.java
@@ -4,23 +4,45 @@ public class TextBlocks {
 
     public static void main(String[] args) {
 
+        System.out.println("*** Example1 ***");
         System.out.println(getStrValue());
-
         System.out.println(getStrValueOld());
+
+        System.out.println("\n*** Example2 ***");
+        System.out.println(getHtmlUsingTextBlock("Java"));
+        System.out.println(getHtmlOld("Java"));
 
     }
 
-    public static String getStrValue(){
-        return  """
+    public static String getStrValue() {
+        return """
                 Learning Java.
                   Version :
                   "JDK 17"
                 """;
     }
 
-    public static String getStrValueOld(){
+    public static String getStrValueOld() {
         return "Learning Java\n" +
                 "  Version :\n" +
                 "  \"JDK 17\"";
+    }
+
+    public static String getHtmlUsingTextBlock(String title) {
+        return """
+                <html>
+                    <body>
+                        <h1>%s</h1>
+                    </body>
+                </html>        
+                """.formatted(title);
+    }
+
+    public static String getHtmlOld(String title) {
+        return "<html>\n" +
+                "    <body>\n" +
+                "        <h1>" + title + "</h1>\n" +
+                "    </body>\n" +
+                "</html>\n";
     }
 }
