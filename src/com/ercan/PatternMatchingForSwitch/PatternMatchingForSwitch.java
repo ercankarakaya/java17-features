eaa003ea274d99e8add8aee6338304be54949074
Ercan Karakaya <karakayaercan71@gmail.com>
2023-10-29 00:35:22 +0300

    Pattern matching for switch

--- src/com/ercan/PatternMatchingForSwitch/PatternMatchingForSwitch.java
+++ src/com/ercan/PatternMatchingForSwitch/PatternMatchingForSwitch.java
@@ -20,11 +20,19 @@ public class PatternMatchingForSwitch {
         var result3 = parseDoublePatternMatching("21");
         System.out.println(result3);
 
+        System.out.println("*** Example4 ****");
+        var result4 = numberOfLettersSwitch(Day.FRIDAY);
+        System.out.println(result4);
+
     }
+
+    /**
+     * This method has Type Pattern. There is a Type Pattern in all 4 cases.
+     */
 
     public static String getObjectSize(Object obj) {
         return switch (obj) {
-            case String s -> "A string of length : " + s.length();
+            case String s -> "A string of length : " + s.length(); // Type Pattern
             case List list -> "List of size : " + list.size();
             case Map map -> "Map of entry size : " + map.size();
             case Set set -> "Set of element size : " + set.size();
@@ -32,11 +40,15 @@ public class PatternMatchingForSwitch {
         };
     }
 
+    /**
+     * In this method, there are both Type Pattern and Guarded Pattern. There is Guarded in only 1 case.
+     */
+
     public static String getObjectSize2(Object obj) {
         return switch (obj) {
             case String s -> "A string of length : " + s.length();
             case List list -> "List of size : " + list.size();
-            case Map map && map.size() > 2 -> "Map is too large";
+            case Map map && map.size() > 2 -> "Map is too large"; // Guarded Pattern
             case Map map -> "Map of entry size : " + map.size();
             case Set set -> "Set of element size : " + set.size();
             default -> throw new IllegalStateException("Unexpected value: " + obj);
@@ -48,6 +60,15 @@ public class PatternMatchingForSwitch {
             case String s && s.length() > 0 -> Double.parseDouble(s);
             case Number n -> n.doubleValue();
             default -> 0d;
+        };
+    }
+
+    public static int numberOfLettersSwitch(Day day) {
+        return switch (day) {
+            case MONDAY, FRIDAY, SUNDAY -> 6;
+            case TUESDAY -> 7;
+            case THURSDAY, SATURDAY -> 8;
+            case WEDNESDAY -> 9;
         };
     }
 
@@ -76,5 +97,38 @@ public class PatternMatchingForSwitch {
             return ((Number) obj).doubleValue();
         }
         return 0d;
+    }
+
+    public static int numberOfLettersOld(Day day) {
+        int letters = 0;
+        switch (day) {
+            case MONDAY:
+            case FRIDAY:
+            case SUNDAY:
+                letters = 6;
+                break;
+            case TUESDAY:
+                letters = 7;
+                break;
+            case THURSDAY:
+            case SATURDAY:
+                letters = 8;
+                break;
+            case WEDNESDAY:
+                letters = 9;
+                break;
+        }
+        return letters;
+    }
+
+    /** Enum */
+    enum Day {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
     }
 }
